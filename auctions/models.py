77339b5fad336c06e70c766f78ec1d8b0887feef
DanielAdeyemi <adeyemidany@gmail.com>
2022-08-24 19:40:49 -0700

    start models creation

--- auctions/models.py
+++ auctions/models.py
@@ -4,3 +4,21 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+
+class Comment(models.Model):
+    comments = models.CharField()
+
+    def __str__(self):
+        return f"{self.comments}"
+
+
+class Bid(models.Model):
+    initial_bid = models.DecimalField(decimal_places=2)
+
+    def __str__(self):
+        return f"{self.initial_bid}"
+
+
+class Listing(models.Model):
+    pass
