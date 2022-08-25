183406f4dde4f422df8c578f347f931621bf5756
DanielAdeyemi <adeyemidany@gmail.com>
2022-08-25 12:20:20 -0700

    create and migrate models for the auction app

--- auctions/models.py
+++ auctions/models.py
@@ -7,18 +7,27 @@ class User(AbstractUser):
 
 
 class Comment(models.Model):
-    comments = models.CharField()
+    comment = models.TextField()
 
     def __str__(self):
-        return f"{self.comments}"
+        return f"{self.comment}"
 
 
 class Bid(models.Model):
-    initial_bid = models.DecimalField(decimal_places=2)
+    initial_bid = models.DecimalField(decimal_places=2, max_digits=10)
 
     def __str__(self):
         return f"{self.initial_bid}"
 
 
 class Listing(models.Model):
-    pass
+    title = models.CharField(max_length=64)
+    description = models.TextField()
+    picture = models.URLField()
+    creation_time = models.DateTimeField(auto_now_add=True)
+    current_bid = models.ForeignKey(
+        Bid, on_delete=models.CASCADE, related_name='bid')
+    comments = models.ForeignKey(
+        Comment, on_delete=models.CASCADE, related_name='comments')
+    creator = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='creator')
